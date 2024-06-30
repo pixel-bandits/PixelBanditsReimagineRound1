673110cecc03ddd017c6a32ba76227a35624ae1b
Brishti Das <das.brishti342@gmail.com>
2024-06-30 21:13:47 +0530

    made footer responsive

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -7,25 +7,26 @@ import {
   faXTwitter,
   faYoutube,
 } from "@fortawesome/free-brands-svg-icons";
+
 const Footer = () => {
   return (
     <div className="panel">
-      <div className="h-[100vh] mx-auto">
-        <div className="cta w-[40%] mx-auto">
-          <h3 className="text-xl font-semibold my-5 text-center">
+      <div className="container mx-auto px-4 py-8">
+        <div className="cta w-full md:w-2/3 lg:w-1/2 mx-auto text-center">
+          <h3 className="text-xl font-semibold my-5">
             SUBSCRIBE TO OUR NEWSLETTER
           </h3>
-          <div className="w-full">
+          <div className="w-full flex flex-col md:flex-row justify-center items-center gap-4">
             <input
               type="email"
               id="email"
               placeholder="Email"
-              className="border-b-neutral-400 border-b-[1px] py-[9px] focus:outline-none w-[72%]"
+              className="border-b-neutral-400 border-b-[1px] py-2 focus:outline-none w-full md:w-2/3"
             />
-            <button className="btn">Subscribe</button>
+            <button className="btn w-full md:w-auto">Subscribe</button>
           </div>
         </div>
-        <div className="pageLinks flex justify-between max-w-[1200px] mx-auto my-10">
+        <div className="pageLinks flex flex-col md:flex-row justify-between max-w-[1200px] mx-auto my-10 gap-10">
           <div>
             <h4 className="text-lg font-semibold">Collections</h4>
             <ul>
@@ -56,7 +57,6 @@ const Footer = () => {
               </li>
             </ul>
           </div>
-
           <div>
             <h4 className="text-lg font-semibold">Contact Us</h4>
             <ul>
@@ -67,29 +67,38 @@ const Footer = () => {
               <li>Help & Contact</li>
             </ul>
           </div>
-
           <div className="socials">
             <h4 className="text-lg font-semibold mb-4">Follow us</h4>
-            <FontAwesomeIcon icon={faFacebook} size="lg" className="hover" />
-            <FontAwesomeIcon
-              icon={faInstagram}
-              className="icon hover"
-              size="lg"
-            />
-            <FontAwesomeIcon icon={faXTwitter} size="lg" className="hover" />
-            <FontAwesomeIcon
-              icon={faYoutube}
-              size="lg"
-              className="ml-[20px] hover"
-            />
+            <div className="flex gap-4 justify-center md:justify-start">
+              <FontAwesomeIcon
+                icon={faFacebook}
+                size="lg"
+                className="hover:text-blue-600"
+              />
+              <FontAwesomeIcon
+                icon={faInstagram}
+                size="lg"
+                className="hover:text-pink-600"
+              />
+              <FontAwesomeIcon
+                icon={faXTwitter}
+                size="lg"
+                className="hover:text-blue-500"
+              />
+              <FontAwesomeIcon
+                icon={faYoutube}
+                size="lg"
+                className="hover:text-red-600"
+              />
+            </div>
           </div>
         </div>
-        <div className="copyright flex justify-between">
-          <div className="flex justify-between w-[27%] ">
-            <p className="hover">Terms and Conditions</p>
-            <p className="hover">Privacy Policy</p>
+        <div className="copyright flex flex-col md:flex-row justify-between items-center text-center md:text-left gap-4">
+          <div className="flex flex-col md:flex-row gap-4">
+            <p className="hover:underline">Terms and Conditions</p>
+            <p className="hover:underline">Privacy Policy</p>
           </div>
-          <p className="hover">
+          <p className="hover:underline">
             &copy; 2024 Titan Company Limited. All Rights Reserved.
           </p>
         </div>
